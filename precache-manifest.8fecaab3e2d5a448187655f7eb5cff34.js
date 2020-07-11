95bf1dea02d5e9a119d37f0ee651bde9a6d8d6d6
Zach Lind <zlind@ucsc.edu>
2020-07-11 10:35:43 -0700

    Updates

---
+++ precache-manifest.8fecaab3e2d5a448187655f7eb5cff34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1cc8437ada236d9db53269a70241155",
+    "url": "/notifications/index.html"
+  },
+  {
+    "revision": "7a91a759322d4ef250f8",
+    "url": "/notifications/static/css/2.daf4b330.chunk.css"
+  },
+  {
+    "revision": "42deb2baf75059c6d5c2",
+    "url": "/notifications/static/css/main.3408f53f.chunk.css"
+  },
+  {
+    "revision": "7a91a759322d4ef250f8",
+    "url": "/notifications/static/js/2.36081d9c.chunk.js"
+  },
+  {
+    "revision": "2abff51e4842615196de37b9e1f7bf38",
+    "url": "/notifications/static/js/2.36081d9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42deb2baf75059c6d5c2",
+    "url": "/notifications/static/js/main.61b0863e.chunk.js"
+  },
+  {
+    "revision": "60502e68c6f1e2bfb275",
+    "url": "/notifications/static/js/runtime-main.4821dba3.js"
+  }
+]);
